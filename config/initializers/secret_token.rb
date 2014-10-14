e3749b62defbd672bb347c124719fba30ff0ad2e
Richard Reed <rareed@uncc.edu>
2014-10-14 16:20:09 -0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myrottenpotatoes::Application.config.secret_token = 'f9c5f764baeeb0e4888bf2678bb275a28c3afcb86468c49d652e5ccc459bcd2cafe104b9ff233c367ed50d25ecec496c8cd77a99d7415b53279baa3430f6eea9'
